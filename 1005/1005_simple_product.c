5a6317e62b3368d28d51c17cee6b1f4d95ad67a6
Leonardo Farias <leofariasrj25@gmail.com>
2021-08-19 16:10:20 -0300

    1004 - simple product

---
+++ 1005/1005_simple_product.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  int num_a, num_b, product;
+
+  scanf("%d", &num_a);
+  scanf("%d", &num_b);
+
+  product = num_a * num_b;
+
+  printf("PROD = %d\n", product);
+
+  return 0;
+}
